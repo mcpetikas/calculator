82353f787522ddb50f154d7628adb9da278febf1
mcpetikas <77706491+mcpetikas@users.noreply.github.com>
2021-02-16 21:48:29 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+let runningTotal;
+let currentInput;
+let toAdd;
+
+function updateDisplay () {
+
+}
+
+function setHeldOperation () {
+
+}
+
+function setRunningTotal () {
+
+}
+
+function ClearInput () {
+// $(".clear-entry").click()
+// currentInput = 0;
+}
+
+function ClearAll () {
+// $(".clear-all").click()
+// runningTotal = 0;
+// currentInput = 0;
+}
